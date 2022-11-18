b745715ad3a89fd0023d1aa5768b336697fe4092
AbeTetsuya20 <tetsuya@abetetsuya20.com>
2022-11-19 03:08:30 +0900

    seed ファイルの作成

--- cmd/main.go
+++ cmd/main.go
@@ -2,19 +2,63 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"github.com/go-sql-driver/mysql"
+	"log"
+	"net"
+	"os"
+	"strconv"
 )
 
 func main() {
 	fmt.Println("run main.go")
 
-	_ = context.Background()
+	ctx := context.Background()
 
 	// database の初期化
+	sql, err := InitDatabase("localhost", 3306, "app", "root", "password")
+	if err != nil {
+		fmt.Println("error DB Open")
+		os.Exit(1)
+	}
+
+	_, err = sql.ExecContext(ctx, "use app")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = sql.ExecContext(ctx, "CREATE TABLE IF NOT EXISTS user (id integer, name varchar(30))")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	query := "INSERT INTO user (id, name) VALUES (?, ?)"
+	_, err = sql.ExecContext(ctx, query, 1234, "test")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
 	// interface の初期化
 
 	// サーバーの起動
 
 	fmt.Println("finish main.go")
+}
+
+func InitDatabase(host string, port uint16, dbname, username, password string) (*sql.DB, error) {
+	cfg := mysql.NewConfig()
+	cfg.Addr = net.JoinHostPort(host, strconv.Itoa(int(port)))
+	cfg.DBName = dbname
+	cfg.User = username
+	cfg.Passwd = password
+	cfg.ParseTime = true
+
+	connector, err := mysql.NewConnector(cfg)
+	if err != nil {
+		return nil, fmt.Errorf("new connector: %w", err)
+	}
+
+	return sql.OpenDB(connector), nil
 }
--- go.mod
+++ go.mod
@@ -2,9 +2,4 @@ module github.com/AbeTetsuya20/ddd_challenge
 
 go 1.19
 
-require (
-	github.com/go-chi/chi v1.5.4
-	github.com/go-chi/render v1.0.2
-)
-
-require github.com/ajg/form v1.5.1 // indirect
+require github.com/go-sql-driver/mysql v1.6.0
